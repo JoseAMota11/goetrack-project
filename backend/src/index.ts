a85fb60879066b319e1cf50669c184b24152c026
José Mota <jose.a.mota11@gmail.com>
2024-11-16 17:51:49 -0400

    :heavy_plus_sign: Adding cors dependecy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import peopleRoutes from './routes/people.route';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 // Middlewares
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+);
 app.use(express.json());
 
 app.use('/api/v1', peopleRoutes);
