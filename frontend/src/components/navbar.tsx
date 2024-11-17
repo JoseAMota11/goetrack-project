0667870bd7286bcff55a2ecec7c9aae1c6fd2e74
José Mota <jose.a.mota11@gmail.com>
2024-11-17 15:35:31 -0400

    :construction: Installing and adding tailwindcss and initial structure

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className="bg-white px-4 py-6 rounded-md shadow-md">
+      <header>
+        <h1 className="text-xl font-bold">Geotrack</h1>
+      </header>
+    </nav>
+  );
+}
+
+export default Navbar;
